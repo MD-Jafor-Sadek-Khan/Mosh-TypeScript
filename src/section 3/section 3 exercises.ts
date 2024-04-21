d2094ca510298425ab51ccd541afb1175986263b
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-04-21 22:08:22 +0600

    section 3 exercise done

---
+++ src/section 3/section 3 exercises.ts
@@ -0,0 +1,63 @@
+// Given the data below, define a type alias for representing users.
+
+type User = {
+  name: string
+  age: number
+  occupation?: string
+}
+type Users = User[]
+
+let users: Users = [
+  {
+    name: "John Smith",
+    age: 30,
+    occupation: "Software engineer",
+  },
+  { name: "Kate Müller", age: 28 },
+]
+
+// • Birds fly. Fish swim. A Pet can be a Bird or Fish. Use type aliases to represent these
+
+type Birds = {
+  fly: () => void
+}
+
+type Fish = {
+  swim: () => void
+}
+
+type Pet = Birds | Fish
+
+let pet: Pet = {
+  fly() {},
+}
+
+// Define a type for representing the days of week. Valid values are “Monday”, “Tuesday”,
+// etc.
+
+type Days = "monday" | "tuesday" | "wednesday" | "friday"
+
+let day: Days = "wednesday"
+
+// Simplify the following code snippets:
+
+let user = getUser()
+console.log(user && user.address ? user.address.street : undefined)
+// sol
+let user = getUser()
+console.log(user?.address?.street)
+
+let x = foo !== null && foo !== undefined ? foo : bar()
+// sol
+let x = foo ?? bar()
+
+// What is the problem in this piece of code?
+let value: unknown = "a"
+console.log(value.toUpperCase())
+
+// we assigned type unknown to value even though we assigned a string to it. now the compiler is confused it the value has string methods or not.
+
+// sol by type narrowing
+
+let value: unknown = "a"
+if (typeof value === "string") console.log(value.toUpperCase())
