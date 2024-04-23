411bac29a2d372646c77a580c292af849bb6f66f
MD-Jafor-Sadek-Khan <rksadeck@gmail.com>
2024-04-24 00:27:14 +0600

    added ReminderServices class with getReminders, postReminder and deleteReminder methods using Axios

--- src/section 9/React with TypeScript/reminders-app/src/App.tsx
+++ src/section 9/React with TypeScript/reminders-app/src/App.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./App.css"
 import ReminderList from "./Components/ReminderList.component"
 import Reminder from "./Models/Reminder"
+import reminderService from "./Services/Reminder"
 
 function App() {
-  const [reminderList, setReminderList] = useState<Reminder[]>([
-    { id: 1, title: "Reminder 1" },
-  ])
+  const [reminderList, setReminderList] = useState<Reminder[]>([])
+
+  useEffect(() => {
+    fetchReminders()
+  }, [])
+
+  async function fetchReminders() {
+    const fetchedRemindersList = await reminderService.getReminders()
+    setReminderList(fetchedRemindersList)
+  }
 
   return (
     <div className="App">
---
+++ src/section 9/React with TypeScript/reminders-app/src/Services/Reminder.ts
@@ -0,0 +1,25 @@
+import axios from "axios"
+import Reminder from "../Models/Reminder"
+
+class ReminderService {
+  http = axios.create({
+    baseURL: "https://jsonplaceholder.typicode.com/",
+  })
+
+  async getReminders() {
+    const response = await this.http.get<Reminder[]>("/posts")
+    return response.data
+  }
+
+  async postReminder(title: string) {
+    const response = await this.http.post<Reminder>("/posts")
+    return response.data
+  }
+
+  async deleteReminder(id: number) {
+    const response = await this.http.delete("/posts/" + id)
+    return response.data
+  }
+}
+
+export default new ReminderService()
